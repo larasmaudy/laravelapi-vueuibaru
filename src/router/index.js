2c6acf0a2052b62001d275b24c90e218592eb96f
larasmaudy <larasmaudy66@gmail.com>
2021-06-29 21:30:35 +0700

    commit Pertemuan 9

---
+++ src/router/index.js
@@ -0,0 +1,53 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import Createfriends from '../views/Createfriends.vue'
+import Editfriends from '../views/Editfriends.vue'
+import Detailfriends from '../views/friends/Detailfriends.vue'
+import Groups from '../views/groups/Index.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/createfriends',
+    name: 'Createfriends',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: Createfriends
+  },
+  {
+    path: '/editfriends',
+    name: 'Editfriends',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: Editfriends
+  },
+  {
+    path: '/detailfriends/:id',
+    name: 'Detailfriends',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: Detailfriends
+    },
+    {
+    path: '/groups',
+    name: 'groups.index',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: Groups
+    }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
